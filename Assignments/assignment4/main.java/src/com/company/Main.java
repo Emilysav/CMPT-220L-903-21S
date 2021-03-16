990e6326ea6ae36228646294b11674b31e8f717e
Emilysav <70286291+Emilysav@users.noreply.github.com>
2021-03-15 21:32:37 -0400

    assignment 4

---
+++ Assignments/assignment4/main.java/src/com/company/Main.java
@@ -0,0 +1,15 @@
+// Create a function named summation that will take in an int[ ] and return an int. I want the
+// function to then go through the given array and sum up digits in the array. Once that’s
+// done return the answer.
+// From the main function call your new summation function with an example array and
+// print out the answer
+
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
