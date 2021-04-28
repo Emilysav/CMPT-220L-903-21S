4bd7a271aa2f3bb24ce052c74b1387713bb2696d
Emilysav <70286291+Emilysav@users.noreply.github.com>
2021-04-28 11:38:48 -0400

    7

--- Assignments/assignment 7/src/base/Main.java
+++ Assignments/assignment 7/src/base/Main.java
@@ -1,8 +1,20 @@
 package base;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] array = {8, 4, 6, 5, 3, 9, 11};
+
+	    for (int i = 0,  size = array.length; i <size -1; i++){
+			for (int j = 0; j < size - 1 - i; j++)
+				if (array[j] > array[j+1]){
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
